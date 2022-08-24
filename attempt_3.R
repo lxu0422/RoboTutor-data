ad3213ff773b3b3cd0818fc90a4a0ab5f9f2b356
lxu0422 <108370532+lxu0422@users.noreply.github.com>
2022-08-24 01:39:20 -0700

    Add files via upload

--- attempt_3.R
+++ attempt_3.R
@@ -61,7 +61,7 @@ data_new <- data_1[data_1$activity_duration < max_,]
 #2
 ggplot(data_new,aes(x=jitter(start_timestamp),y=activity_duration))+geom_point()
 # Calculate the proportion of the number of 0s
-sum(data_new$activity_duration == 0) / length(data_new$activity_duration)
+sum(data_new$activity_duration == 0)*100 / length(data_new$activity_duration)
 # 
 hist(data_new[data_new$activity_duration != 0,]$activity_duration, xlab = "activity_duration", main = "Distribution of activity_duration")
 
@@ -79,6 +79,11 @@ ggplot(data_new,aes(x=Area_AN,y=Percentage_Completion, col = color, size = Perce
   ylim(0,100)
 
 hist(data_new$Percentage_Completion, xlab = "Percentage_Completion", main="Histgorm of Percentage_Completion")
+
+df3<-data_new
+sum(df3$Percentage_Completion>=0 & df3$Percentage_Completion<=100)*100/length(df3$Percentage_Completion)
+sum(df3$Percentage_Completion>100)*100/length(df3$Percentage_Completion)#bailout rate
+
 #4
 #Time divided by the completion rate, which represents the time each child completed in full
 x <- data_new$activity_duration / data_new$Percentage_Completion
@@ -91,7 +96,7 @@ df5 %>%
   group_by(Area_AN) %>%
   summarise(
     completed_count = sum(Percentage_Completion >= 100, na.rm=TRUE), 
-    completed_percentage = completed_count / nrow(df5)) %>%
+    completed_percentage = completed_count*100 / nrow(df5)) %>%
   knitr::kable()
 
 #6
@@ -105,8 +110,21 @@ df5 %>%
 #7
 data_new$same <- ifelse(gsub(":","_",data_new$ActivityName) == gsub(" ","",data_new$TutorID), 1, 0)
 View(data_new)
+hist(data_new$same, freq = TRUE, labels=TRUE,ylim = c(0,2000))
 
 #8
+ggplot(data_new,aes(x=completion)) + geom_bar()
+
+data_new %>% 
+  count(completion) %>% 
+  mutate(perc = n / nrow(data_new)) -> df8
+
+ggplot(df8, aes(x = completion, y = perc)) + geom_bar(stat = "identity")+
+  geom_text(aes(label=paste0(sprintf("%.2f", perc*100),"%")),position = position_dodge(width = .9),vjust = -0.5,size = 5)+
+  scale_y_continuous(labels = scales::percent)
+  
+
+#9
 #Boxplot: activity duration on Area_AN
 boxplot(data_new$activity_duration~data_new$Area_AN)
 #Pie plot
@@ -117,3 +135,4 @@ legend("topright", c("DNFT", "literacy", "math", "story"), cex = 1,
        fill = rainbow(length(df$percent)))
 # density plot
 ggplot(data_new, aes(x = Percentage_Completion))+ geom_density(color = 'black', fill = 'gray')
+
